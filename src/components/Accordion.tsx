d16070a55db250b93bef6ff03b10097c66ac4128
Viktoriyadegt <vika.degtyareva.12@mail.ru>
2023-03-26 15:17:50 +0300

    ratting-accordion-app

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Accordion = () => {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+
+        </div>
+    );
+};
+
+export default Accordion;
---
+++ src/components/Ratting.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Ratting = () => {
+    return (
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
+    );
+};
+
+export default Ratting;
